a9da6e809f9f299efb67ca57c2103deb657da3ed
Huy Tran <max@gotitapp.co>
2021-11-10 17:39:35 +0700

    fix: Check src dir

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 if [ $CODEBUILD_BUILD_SUCCEEDING -eq 1 ]
 then
   echo "Deploying to s3://$S3"
-  npm run build
+  echo "Sync..."
+  ls $CODEBUILD_SRC_DIR
   aws s3 sync --delete --cache-control max-age=2592000,public $CODEBUILD_SRC_DIR/build/ s3://$S3
+  echo "Invalidation"
   aws cloudfront create-invalidation --distribution-id $CLOUDFRONT --paths "/*"
 fi
