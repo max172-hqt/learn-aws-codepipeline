26583e1ab34d2545e7e6c09ddecf12cbe733363a
Huy Tran <max@gotitapp.co>
2021-11-10 16:40:27 +0700

    feat: Use deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
-echo "Deploying to s3://$S3"
-npm run build
-aws s3 sync --delete --cache-control max-age=2592000,public ./build/ s3://$S3
-aws cloudfront create-invalidation --distribution-id $CLOUDFRONT --paths "/*"
+if [ $CODEBUILD_BUILD_SUCCEEDING -eq 1 ]
+then
+  echo "Deploying to s3://$S3"
+  npm run build
+  aws s3 sync --delete --cache-control max-age=2592000,public $CODEBUILD_SRC_DIR/build/ s3://$S3
+  aws cloudfront create-invalidation --distribution-id $CLOUDFRONT --paths "/*"
+fi
