2f2bff045cf654b1dfb325ab37ab0eca1ae1651e
Huy Tran <max@gotitapp.co>
2021-11-11 16:54:08 +0700

    feat: Another run

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React - Test codepipeline - Should work
+          Learn React - Test codepipeline - Should work - Another run
         </a>
       </header>
     </div>
